09137d8ec7c564a527e4db779cd8eb04f0494319
Marcel Medeiros <131443301+marcelmedeiros1@users.noreply.github.com>
2023-11-17 16:30:08 +0000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <DabbleESP32.h>
 #include <MotorControl.h>
 
-#define maxSpeed 150
+#define maxSpeed 70
 // Select the motors to control
 MotorControl motor3(3);
 MotorControl motor4(4);
+
+int y_axis, x_axis, motor3_speed, motor4_speed;
 
 void stopMotors(){
     motor3.setSpeed(0);
@@ -26,27 +28,42 @@ void loop() {
     int x_axis = GamePad.getXaxisData(); // Obtém dados do eixo X
     int y_axis = GamePad.getYaxisData(); // Obtém dados do eixo Y
 
-    int motor3_speed = y_axis * maxSpeed / 7;
-    int motor4_speed = y_axis * maxSpeed / 7;
+    int motor3_speed = y_axis * (maxSpeed / 7);
+    int motor4_speed = y_axis * (maxSpeed / 7);
+
+    motor4.setSpeed(motor4_speed);
+    motor3.setSpeed(motor3_speed);
 
     motor4_speed = constrain(motor4_speed + x_axis * maxSpeed / 7, -maxSpeed, maxSpeed);
     motor3_speed = constrain(motor3_speed - x_axis * maxSpeed / 7, -maxSpeed, maxSpeed);
-    // Define as velocidades dos motores
-    motor3.setSpeed(motor3_speed);
-    motor4.setSpeed(motor4_speed);
-
     // Move os motores de acordo com as velocidades definidas
     if (y_axis > 0) {
-        motor3.forward();
-        motor3.setSpeed(motor3_speed);
-        motor4.forward();
-        motor4.setSpeed(motor4_speed);
-    } else if (y_axis < 0) {
-        motor3.backward();
-        motor3.setSpeed(motor3_speed);
-        motor4.backward();
-        motor4.setSpeed(motor4_speed);
-    } else {
-        stopMotors();
-    }
+    motor3.forward();
+    motor3.setSpeed(motor3_speed);
+    motor4.forward();
+    motor4.setSpeed(motor4_speed);
+} else if (y_axis < 0) {
+    motor3.backward();
+    motor3.setSpeed(motor3_speed);
+    motor4.backward();
+    motor4.setSpeed(motor4_speed);
+} else if (y_axis == 0 && x_axis == 0) {
+    stopMotors();
+}
+
+    Serial.println("x_axis: ");
+    Serial.println(GamePad.getXaxisData());
+    Serial.println('\n');
+
+    Serial.println("y_axis: ");
+    Serial.println(GamePad.getYaxisData());
+    Serial.println('\n');
+
+    Serial.println("motor3_speed: ");
+    Serial.println(motor3_speed);
+    Serial.println('\t');
+
+Serial.print("motor4_speed: ");
+Serial.println(motor4_speed);
+
 }
